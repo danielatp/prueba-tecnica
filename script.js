b496904798d52bf07724ce147316b42bf5895864
Daniela Tizon <dtizonportilla@gmail.com>
2018-03-18 23:35:54 -0500

    added mergeSort function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var NUMBERS = []
 
 function checkNumber(n){
-  return n !== '' && NUMBERS.indexOf(n) == -1
+  return n !== '' && NUMBERS.indexOf(n) == -1 && n !== '-0'
 }
 
 function addNumber(){
@@ -28,16 +28,42 @@ function clearAllNumbers(){
   NUMBERS = []
 }
 
-function sortNumbers(arr){
-  //will change sort function later
-  return arr.sort()
+function split(wholeArray) {
+  var firstHalf = wholeArray.slice(0, Math.abs(wholeArray.length/2))
+  var secondHalf = wholeArray.slice(Math.abs(wholeArray.length/2))
+  return [firstHalf, secondHalf]
+}
+
+function merge(arr1, arr2){
+  var output = []
+  while(arr1.length || arr2.length){
+    if(!arr1.length){
+      return output.concat(arr2)
+    }
+    if(!arr2.length){
+      return output.concat(arr1)
+    }
+    if(arr1[0] > arr2[0]){
+      output.push(arr2.shift());
+    }else{
+      output.push(arr1.shift())
+    }
+  }
+  return output;
+}
+
+function mergeSort(arr){
+  if(arr.length < 2) return arr
+  else {
+    var temp = split(arr)
+    return merge(mergeSort(temp[0]), mergeSort(temp[1]))
+  }
 }
 
 function reorderNumbers(){
   var divArray = [...document.getElementById('numbers').children]
   var numArray = divArray.map( elem => Number(elem.id))
-
-  numArray = sortNumbers(numArray)
+  numArray = mergeSort(numArray)
   for(var i = 0; i < numArray.length; i++){
     var id = numArray[i]
     document.getElementById(id).style.order = i
