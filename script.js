386eb82bb7eaa12f499721b96b067f7d11df196e
Daniela Tizon <dtizonportilla@gmail.com>
2018-03-19 00:14:37 -0500

    agregué mensajes de error

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var NUMBERS = []
 
 function checkNumber(n){
-  return n !== '' && NUMBERS.indexOf(n) == -1 && n !== '-0'
+  var p = document.getElementById('message')
+  if(!Number(n)){
+    p.innerHTML = 'Solo números!!'
+  }
+  if(NUMBERS.indexOf(n) > -1){
+    p.innerHTML = 'yala'
+  }
+  if(n === '-0'){
+    p.innerHTML = '-0???'
+  }
+  return Number(n) && NUMBERS.indexOf(n) == -1 && n !== '-0'
 }
 
 function addNumber(){
+  document.getElementById('message').innerHTML = ''
   var input = document.getElementById('number-input')
   var numberInput = input.value;
 
