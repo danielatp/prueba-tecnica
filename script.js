2e8a544fdd8383b3fb000a15158a9efb9e86ae03
Daniela Tizon <dtizonportilla@gmail.com>
2018-03-18 23:01:24 -0500

    numbers can be re-ordered, missing sort function without sort

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-
-function sort(arr){
-  var arrCopy = [...arr]
-  return arrCopy.sort()
-}
-
-
-const testArr = [1,5,2,3,7,6,4]
-const sortArr = sort(testArr)
-
-console.log('RESULTADO PRUEBA \ntestArr:', testArr, '\nsortArr:', sortArr)
-
-//////////////////////////////////////////////////////////////////////////////
-
-var NUMBERS = [];
+var NUMBERS = []
 
 function checkNumber(n){
   return n !== '' && NUMBERS.indexOf(n) == -1
@@ -23,25 +9,42 @@ function addNumber(){
   var numberInput = input.value;
 
   if(checkNumber(numberInput)){
-    NUMBERS.push(numberInput)
-    // console.log(NUMBERS)
     var node = document.createElement('div')
     node.className = 'number'
     node.id = numberInput
     node.innerHTML = numberInput
 
     document.getElementById('numbers').appendChild(node)
+    NUMBERS.push(numberInput)
   }
   input.value = '';
 }
 
-function clearNumbers(){
+function clearAllNumbers(){
   var parent = document.getElementById('numbers')
   while(parent.firstChild){
     parent.removeChild(parent.firstChild)
   }
+  NUMBERS = []
+}
+
+function sortNumbers(arr){
+  //will change sort function later
+  return arr.sort()
+}
+
+function reorderNumbers(){
+  var divArray = [...document.getElementById('numbers').children]
+  var numArray = divArray.map( elem => Number(elem.id))
+
+  numArray = sortNumbers(numArray)
+  for(var i = 0; i < numArray.length; i++){
+    var id = numArray[i]
+    document.getElementById(id).style.order = i
+  }
 }
 
 document.getElementById('submit').addEventListener('click',addNumber)
-document.getElementById('clear').addEventListener('click', clearNumbers)
+document.getElementById('clear').addEventListener('click', clearAllNumbers)
+document.getElementById('sort').addEventListener('click', reorderNumbers)
 
